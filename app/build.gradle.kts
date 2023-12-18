45b38570a8fd1b074e405e890890caa58a69986c
vahidmohammadisan <vahidmohammadisan@gmail.com>
2023-12-18 21:12:32 +0330

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("template.android.app")
+    id("template.android.compose")
+    id("template.android.hilt")
+}
+
+android {
+    namespace = "ir.vahidmohammadisan.template"
+    defaultConfig {
+        applicationId = "ir.vahidmohammadisan.template"
+    }
+}
+
+dependencies {
+
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google()
+        maven(
+            uri("https://maven.google.com")
+        )
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    includeBuild("build-logic")
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        maven(
+            uri("https://maven.google.com")
+        )
+        google()
+        maven(
+            uri("https://maven.google.com")
+        )
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Template"
+include(":app")
+ 
